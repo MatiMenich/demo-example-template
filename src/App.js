1c08f8ce94fa844a76c5cb3bb89f8e4ff83c68ab
Matías Menich <matimenich@gmail.com>
2020-08-20 18:07:13 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        '/api/hello',
+      );
+      setData(result.data);
+    }
+    fetchData();
+  }, []); // Or [] if effect doesn't need props or state
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {JSON.stringify(data)}
     </div>
   );
 }
